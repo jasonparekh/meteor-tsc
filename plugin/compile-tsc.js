41d93a2bdfad888f889cf1c2078b6c1f1c1d020d
Jason Parekh <jasonparekh@gmail.com>
2014-06-28 18:37:59 -0400

    CP of cleanup

--- plugin/compile-tsc.js
+++ plugin/compile-tsc.js
@@ -9,113 +9,115 @@ var ts = Npm.require('ts-compiler');
 var fsStat = Future.wrap(fs.stat);
 //storage.initSync();
 
-// "this." allows for modTimes to survive across changes to compile-tsc.js (useful during tsc devel)
-this.modTimesByArch = this.modTimesByArch || {};
+var archs = {};
+function initArch(archName) {
+  archs[archName] = {name: archName};
+  resetCompilationScopedArch(archs[archName]);
+}
 
-var cachedContents = {};
-var cachedErrorsByArch = {};
-
-var tsInputPaths = [];
-var fullPathsToCompileSteps = {};
-var inputPathsToCompileSteps = {};
+var cachedPathContents = {};
 
 var tsErrorRegex = /(.*[.]ts)\((\d+),(\d)+\): (.+)/;
 var placeholderFileName = "main.tsc_placeholder.ts";
 
 Plugin.registerSourceHandler("ts", function (compileStep) {
+  if (typeof(archs[compileStep.archName]) === 'undefined') {
+    initArch(compileStep.arch);
+  }
+
   if (compileStep.inputPath !== placeholderFileName) {
     handleSourceFile(compileStep);
     return;
   }
 
-  var hadMod = checkAgainstModTime(compileStep.arch);
-  if (!hadMod) tsInputPaths = [];
-  compile(compileStep, hadMod);
-  compilationFinished(compileStep.arch);
+  // Typically, Meteor hands us all of the files pertaining to an arch in clumps (i.e. all files for browser, then all files for os), so most compiles are only two phases (client and server).
+  for (var archName in archs) {
+    var arch = archs[archName];
+    if (arch.compileSteps.length == 0) {
+      continue;
+    }
+
+    var hadMod = checkAgainstModTime(arch);
+    compile(arch, compileStep, hadMod);
+    resetCompilationScopedArch(arch);
+  }
 });
 
 // Save the file input path, and return back to Meteor
 function handleSourceFile(compileStep) {
-  // Ensures the placeholder file exists. If the placeholder file is absent, tsInputPaths would never get cleared.
-  if (tsInputPaths.indexOf(compileStep.inputPath) != -1) {
+  var arch = archs[compileStep.arch];
+
+  // This ensures the placeholder file exists. If the placeholder file is absent, we would have never triggered compilation, so the inputPaths would never have gotten cleared.
+  if (arch.inputPaths.indexOf(compileStep.inputPath) != -1) {
     fs.writeFileSync(placeholderFileName, "");
     compileStep.error({message: "Missing required \"" + placeholderFileName + "\" file; it has been created (make sure to add it to your .gitignore). You may have to touch a .ts file to trigger another compilation."});
     return;
   }
 
-  console.log(compileStep.arch, compileStep.inputPath);
-  tsInputPaths.push(compileStep.inputPath);
-  fullPathsToCompileSteps[compileStep._fullInputPath] = compileStep;
-  inputPathsToCompileSteps[compileStep.inputPath] = compileStep;
+  arch.inputPaths.push(compileStep.inputPath);
+  arch.compileSteps.push(compileStep);
+  arch.fullPathToCompileSteps[compileStep._fullInputPath] = compileStep;
 }
 
 function checkAgainstModTime(arch) {
-  modTimesByArch[arch] = modTimesByArch[arch] || {};
-
   var hadModifications = false;
-  tsInputPaths.forEach(function(path) {
+  arch.inputPaths.forEach(function(path) {
     stats = fsStat(path).wait();
-    if (typeof(modTimesByArch[arch][path]) === 'undefined' || modTimesByArch[arch][path].toString() !== stats.mtime.toString()) {
-      console.log(path,"had mods");
+    if (typeof(arch.modTimes[path]) === 'undefined' || arch.modTimes[path].toString() !== stats.mtime.toString()) {
+      console.log(arch.name, path,"had mods");
       hadModifications = true;
     }
 
-    modTimesByArch[arch][path] = stats.mtime;
+    arch.modTimes[path] = stats.mtime;
   });
 
   return hadModifications;
 }
 
-function compile(placeholderCompileStep, hadModifications) {
-  var arch = placeholderCompileStep.arch;
-  cachedErrorsByArch[arch] = cachedErrorsByArch[arch] || [];
-
-  if (tsInputPaths.length == 0) {
-    return;
-  }
-
+function compile(arch, placeholderCompileStep, hadModifications) {
   if (!hadModifications) {
     // Short-circuit via cache
-    tsInputPaths.forEach(function(path) {
-      var compileStep = inputPathsToCompileSteps[path];
-
+    arch.compileSteps.forEach(function(compileStep) {
       compileStep.addJavaScript({
-        path: path + ".js",
-        sourcePath: path,
-        data: cachedContents[path]//storage.getItem(path)
+        path: compileStep.inputPath + ".js",
+        sourcePath: compileStep.inputPath,
+        data: cachedPathContents[path]//storage.getItem(path)
       })
     });
 
     // Replay errors
-    cachedErrorsByArch[arch].forEach(function(args) {
-      recordError(args.err, placeholderCompileStep, args.errorNumber, arch, true);
+    arch.cachedErrorReplays.forEach(function(errReplay) {
+      recordError(errReplay.err, placeholderCompileStep, errReplay.errorNumber, arch, true);
     });
 
     return;
   }
 
-  cachedErrorsByArch[arch] = [];
-  var browser = arch === "browser";
+  console.log("\nCompiling TypeScript " + arch.name + " files...");
+
+  // Clear cached errors since we're about to re-compile
+  arch.cachedErrorReplays = [];
   var errorCount = 0;
-
-  console.log("\nCompiling TypeScript " + (browser ? "client" : "server") + " files...");
-
-  // AFAICT, this is synchronous (and our callback can get called multiple times if there are errors)
-  compileOptions = {
-    'target': (browser ? 'ES3' : 'ES5'),
+  var compileOptions = {
+    'target': (arch.name === 'browser' ? 'ES3' : 'ES5'),
     'skipWrite': true,
     'removeComments': true
   };
-  ts.compile(tsInputPaths, compileOptions, function(err, results) {
+
+  // This is synchronous (and our callback will get called multiple times if there are errors)
+  ts.compile(arch.inputPaths, compileOptions, function(err, results) {
     if (err) {
+      // TODO
       recordError(err, placeholderCompileStep, ++errorCount, arch, false);
-      return;
+      if (typeof(results) === 'undefined') {
+        return;
+      }
     }
 
     results.forEach(function(res) {
       // res.name is the theoretically-generated js filename
       var tsFullPath = res.name.substr(0, res.name.length-2) + "ts";
-      var compileStep = fullPathsToCompileSteps[tsFullPath];
+      var compileStep = arch.fullPathToCompileSteps[tsFullPath];
 
       compileStep.addJavaScript({
         path: compileStep.inputPath + ".js",
@@ -124,18 +126,18 @@ function compile(placeholderCompileStep, hadModifications) {
       });
 
 //      storage.setItem(compileStep.inputPath, res.text);
-      cachedContents[compileStep.inputPath] = res.text;
+      cachedPathContents[compileStep.inputPath] = res.text;
     });
   });
 }
 
 function recordError(err, placeholderCompileStep, errorNumber, arch, isFromCache) {
   if (!isFromCache) {
-    cachedErrorsByArch[arch].push({err: err, errNumber: errorNumber});
+    arch.cachedErrorReplays.push({err: err, errNumber: errorNumber});
   }
 
   if (match = tsErrorRegex.exec(err.toString())) {
-    var compileStep = fullPathsToCompileSteps[match[1]];
+    var compileStep = arch.fullPathToCompileSteps[match[1]];
     if (compileStep) {
       compileStep.error({
         message: match[4],
@@ -155,8 +157,10 @@ function recordError(err, placeholderCompileStep, errorNumber, arch, isFromCache
   });
 }
 
-function compilationFinished(arch) {
-  tsInputPaths = [];
-  fullPathsToCompileSteps = {};
-  inputPathsToCompileSteps = {};
+function resetCompilationScopedArch(arch) {
+  arch.modTimes = {};
+  arch.cachedErrorReplays = [];
+  arch.inputPaths = [];
+  arch.compileSteps = [];
+  arch.fullPathToCompileSteps = {};
 }
