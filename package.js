a43448fad9cdb2a51a9aa3b808f40d4ce7e05f70
Jason Parekh <jasonparekh@gmail.com>
2014-09-12 00:27:10 -0400

    Update to Meteor 0.9 package

--- package.js
+++ package.js
@@ -1,6 +1,9 @@
 
 Package.describe({
-	summary: "Batch (fast) TypeScript compiler for Meteor"
+	summary: "Batch (fast) TypeScript compiler for Meteor",
+  version: "1.0.9",
+  git: "https://github.com/jasonparekh/meteor-tsc.git",
+	version: "1.0.9"
 });
 
 Package._transitional_registerBuildPlugin({
